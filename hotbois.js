bde6d69f418077b3484b9ea48cde142286a46370
LQMemer <42935807+Petmojen@users.noreply.github.com>
2023-01-15 20:49:35 +0100

    Tempature exchange now works, you are welcome :)

---
+++ hotbois.js
@@ -0,0 +1,12 @@
+var celsiusInput = document.getElementById("celsius");
+var fahrenheitInput = document.getElementById("fahrenheit");
+
+celsiusInput.addEventListener("input", () => {
+  var celsiusConversion = (parseFloat(celsiusInput.value)  * 9/5) + 32;
+  fahrenheitInput.value = celsiusConversion.toFixed(1);
+});
+
+fahrenheitInput.addEventListener("input", () => {
+  var fahrenheitConversion = (parseFloat(fahrenheitInput.value) - 32) * 5/9;
+  celsiusInput.value = fahrenheitConversion.toFixed(1);
+});
